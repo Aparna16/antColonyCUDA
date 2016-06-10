d7ed15b4b66f3311cee448faa378893f9939e02c
rodolphorosa <rodolphorosa05@gmail.com>
2016-06-10 12:29:09 -0300

    Alteração do validator.py

--- results/validator.py
+++ results/validator.py
@@ -4,13 +4,21 @@ from pprint import pprint
 def main(inputname):
 	results = open(inputname).readlines()
 
-	vocab = {}
+	cities = {}
+	# skip first two lines 
+	# for each city, verifies its occurrence 
 	for line in results[2:]:
 		try:
-			vocab[int(line)] += 1 
+			cities[int(line)] += 1 
 		except:
-			vocab[int(line)] = 1 
-	pprint (vocab)
+			cities[int(line)] = 1 
+	
+	for city in cities.keys():
+		if cities[city] > 1:
+			print "Solucao encontrada nao eh valida!"
+			return
+	
+	print "Solucao valida!"
 
 if __name__ == "__main__":
 	
